3c59cce6f0d44106a33b2949c2f769a63e1393f6
AdamSinale <adam.sinale@gmail.com>
2026-02-25 03:02:22 +0200

    aboutPage progress

--- frontend/src/pages/about/GeneralInfo.tsx
+++ frontend/src/pages/about/GeneralInfo.tsx
@@ -10,17 +10,19 @@ export function GeneralInfo() {
   return (
     <Box className={styles.headerContent}>
       <img src={aboutPageImage} alt="" className={styles.headerImage} />
-      <Title className={styles.mainTitle}>
-        {isHe ? 'קהילת איתי' : 'Itay Community'}
-      </Title>
-      <Text className={styles.subtitle}>
-        {isHe ? 'מטרת העמותה' : 'Purpose of the Foundation'}
-      </Text>
-      <Text className={styles.description}>
-        {isHe
-          ? 'קהילת איתי היא קהילה של משפחות של גיבורים שנופלו במלחמה.'
-          : 'Itay Community is a community of families of heroes who fell in battle. We are here to support them and their families.'}
-      </Text>
+      <div className={styles.headerText}>
+        <Title className={styles.mainTitle}>
+          {isHe ? 'קהילת איתי' : 'Itay Community'}
+        </Title>
+        <Text className={styles.subtitle}>
+          {isHe ? 'מטרת העמותה' : 'Purpose of the Foundation'}
+        </Text>
+        <Text className={styles.description}>
+          {isHe
+            ? 'קהילת איתי היא קהילה של משפחות של גיבורים שנופלו במלחמה.'
+            : 'Itay Community is a community of families of heroes who fell in battle. We are here to support them and their families.'}
+        </Text>
+      </div>
     </Box>
   )
 }
